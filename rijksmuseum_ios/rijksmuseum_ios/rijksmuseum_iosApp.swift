20e222dc058fc36c0ded147ea3f379967bc76c83
Illia Vlasov <vlasovia@gmail.com>
2024-04-10 17:37:22 +0200

    Initial

---
+++ rijksmuseum_ios/rijksmuseum_ios/rijksmuseum_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  rijksmuseum_iosApp.swift
+//  rijksmuseum_ios
+//
+//  Created by Illia Vlasov on 10.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct rijksmuseum_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ rijksmuseum_ios/rijksmuseum_iosUITests/rijksmuseum_iosUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  rijksmuseum_iosUITestsLaunchTests.swift
+//  rijksmuseum_iosUITests
+//
+//  Created by Illia Vlasov on 10.04.2024.
+//
+
+import XCTest
+
+final class rijksmuseum_iosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
